73fbf71d0871ecc003dc1e2c55b78a962c439905
Brandon Richey <brichey@teladoc.com>
2015-10-14 00:14:24 -0400

    WIP Adding seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,14 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Pxblog.Repo
+alias Pxblog.Role
+alias Pxblog.User
+
+role = %Role{}
+  |> Role.changeset(%{name: "Admin Role", admin: true})
+  |> Repo.insert!()
+
+admin = %User{}
+  |> User.changeset(%{username: "admin", email: "admin@test.com", password: "test", password_confirmation: "test", role_id: role.id})
+  |> Repo.insert!()

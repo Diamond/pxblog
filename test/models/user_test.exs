b3e6b40a9ca3cfffbd5f00fa5f979511a83b5e6b
Brandon Richey <brichey@teladoc.com>
2015-10-15 17:00:59 -0400

    Adding validations to our user model

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -25,4 +25,42 @@ defmodule Pxblog.UserTest do
     changeset = User.changeset(%User{}, %{email: "test@test.com", password: nil, password_confirmation: nil, username: "test"})
     refute Ecto.Changeset.get_change(changeset, :password_digest)
   end
+
+  test "changeset is invalid if password and confirmation do not match" do
+    changeset = User.changeset(%User{}, %{email: "test@test.com", password: "foo", password_confirmation: "bar", username: "test"})
+    refute changeset.valid?
+    assert changeset.errors[:password_confirmation] == "does not match password!"
+  end
+
+  test "changeset is invalid if username is used already" do
+    %User{}
+    |> User.changeset(@valid_attrs)
+    |> Pxblog.Repo.insert!
+    user2 =
+      %User{}
+      |> User.changeset(@valid_attrs)
+    assert {:error, changeset} = Repo.insert(user2)
+    assert changeset.errors[:username] == "has already been taken"
+  end
+
+  test "changeset is invalid if email is used already" do
+    user2_attrs = @valid_attrs |> Map.put(:username, "test2")
+    %User{}
+    |> User.changeset(@valid_attrs)
+    |> Pxblog.Repo.insert!
+    user2 =
+      %User{}
+      |> User.changeset(user2_attrs)
+    assert {:error, changeset} = Repo.insert(user2)
+    assert changeset.errors[:email] == "has already been taken"
+  end
+
+  test "changeset is invalid if password is too short" do
+    attrs = @valid_attrs |> Map.put(:password, "t") |> Map.put(:password_confirmation, "t")
+    changeset =
+      %User{}
+      |> User.changeset(attrs)
+    refute changeset.valid?
+    assert Dict.has_key?(changeset.errors, :password)
+  end
 end

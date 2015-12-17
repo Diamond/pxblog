4dba661ca31dd0b455b78a9545ac9c311d322ba2
Brandon Richey <diamondgfx@gmail.com>
2015-12-17 00:14:38 -0500

    Upgrading the app to be Phoenix v1.1 compatible

--- mix.exs
+++ mix.exs
@@ -32,16 +32,16 @@ defmodule Pxblog.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 1.0.2"},
-     {:phoenix_ecto, "~> 1.1"},
+    [{:phoenix, "~> 1.1"},
+     {:phoenix_ecto, "~> 2.0"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.1"},
+     {:phoenix_html, "~> 2.3"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 1.2"},
+     {:comeonin, "~> 2.0"},
      {:exrm, "~> 0.19.9"},
      {:conform, "~> 0.17.0"},
-     {:ex_machina, "~> 0.5"},
+     {:ex_machina, "~> 0.6"},
      {:earmark, "~> 0.1.19"}]
   end
 end

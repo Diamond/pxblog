e5cf31f0714924cdf73e0f06b81f3033b80be264
Brandon Richey <brichey@teladoc.com>
2016-01-27 14:13:13 -0500

    Catching up a few other updates

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -30,4 +30,9 @@ defmodule Pxblog.UserTest do
     changeset = User.changeset(%User{}, @valid_attrs)
     assert Comeonin.Bcrypt.checkpw(@valid_attrs.password, Ecto.Changeset.get_change(changeset, :password_digest))
   end
+
+  test "changeset is invalid if password and confirmation do not match" do
+    changeset = User.changeset(%User{}, %{email: "test@test.com", password: "foo", password_confirmation: "bar", username: "test"})
+    refute changeset.valid?
+  end
 end
